64ae041a62d7be0a359d5e892b537e799b236512
Mukaroma <mukaromah.2405356@students.um.ac.id>
2025-06-08 21:59:25 +0700

    Update Final Version

--- static/js/script.js
+++
@@ -1,4 +0,0 @@
-// search_engine_project/static/js/script.js
-// Untuk proyek sederhana ini, tidak ada kode JavaScript yang diperlukan secara default.
-// Kamu bisa menambahkan fungsionalitas JavaScript di sini nanti jika ingin,
-// misalnya validasi form atau efek UI.
